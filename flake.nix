2717c4824409ac0cb485c9aaac21a809cd1c40bb
SamueleFacenda <samuele.facenda@gmail.com>
2025-01-22 13:41:27 +0100

    Big refactor, use scikit-build-core instead of setuptools mixed with cmake in a very hacky and dirty way
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- flake.nix
+++ flake.nix
@@ -18,35 +18,36 @@
 
         packages = rec {
           default = visualizer;
-          visualizer = pkgs.python3.pkgs.toPythonModule ( pkgs.stdenv.mkDerivation {
+          visualizer = pkgs.python3.pkgs.buildPythonPackage {
             pname = "3D-ont";
             src = ./.;
             inherit version;
+            pyproject = true;
+            
+            build-system = with pkgs.python3Packages; [
+              scikit-build-core
+            ];
+            
+            dontUseCmakeConfigure = true;
 
             nativeBuildInputs = with pkgs; [
               libsForQt5.qt5.wrapQtAppsHook
-              cmake
               pkg-config
+              cmake
+              ninja
             ];
             
             buildInputs = with pkgs; [
               eigen
-              python3
               tbb.dev
               libsForQt5.qt5.qtbase
               libGL
-              python3Packages.numpy
             ];
             
-            propagatebBuildInputs = with pkgs.python3Packages; [
+            dependencies = with pkgs.python3Packages; [
               numpy
             ];
-            
-            cmakeFlags = [
-              "-DTBB_ROOT=${pkgs.tbb.out}"
-              "-DTBB_INCLUDE_DIR=${pkgs.tbb.dev}/include"
-            ];
-          });
+          };
         };
 
         apps = {
@@ -59,6 +60,9 @@
         devShells = {
           default = pkgs.mkShell {
             inputsFrom = [ self.packages.${system}.visualizer ];
+            packages = with pkgs.python3Packages; [
+              build
+            ];
           };
         };
       });
--- pptk/__init__.py
+++ pptk/__init__.py
@@ -1,6 +1,7 @@
-from . import _add_path
-from .viewer.viewer import *
-from .points.points import *
-from .points.expr import *
+from .viewer import *
+from .points import *
+from .points import *
 from .kdtree import kdtree
-from .processing.estimate_normals.estimate_normals import estimate_normals
+from .estimate_normals import estimate_normals
+
+from . import _add_path
