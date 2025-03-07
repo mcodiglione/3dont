d148fc992e9aa4efdb411e4e28e63cd9a124393f
SamueleFacenda <samuele.facenda@gmail.com>
2025-03-07 14:10:10 +0100

    Add develop mode for build, doesn't require packaging to test
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- threedont/__main__.py
+++ threedont/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import signal
 
 from threedont import Controller
 
@@ -9,12 +10,13 @@ def main():
 
     controller = Controller()
 
-    try:
-        controller.run()
-    except KeyboardInterrupt:
+    def signal_handler(sig, frame):
         controller.stop()
 
-    controller.stop()
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    controller.run()
 
 if __name__ == '__main__':
     main()
--- threedont/app/controller.py
+++ threedont/app/controller.py
@@ -1,9 +1,28 @@
+from concurrent.futures import ThreadPoolExecutor
 import numpy as np
+import functools
 
 from .viewer import Viewer
 from ..gui import GuiWrapper
 
 __all__ = ['Controller']
+
+# Create a global executor
+executor = ThreadPoolExecutor()
+
+# Define the decorator
+def run_async(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        future = executor.submit(func, self, *args, **kwargs)
+        # Handle errors or result in a callback after the function is done
+        def handle_result(future):
+            if future.exception():
+                print(f"Error in {func.__name__}: {future.exception()}")
+            else:
+                print(f"{func.__name__} completed successfully")
+        future.add_done_callback(handle_result)
+    return wrapper
 
 class Controller:
     def __init__(self):
@@ -18,11 +37,14 @@ class Controller:
         self.gui.run()
 
 
+    @run_async
     def execute_query(self, query):
         print("Controller: ", query)
         # TODO
 
+    @run_async
     def connect_to_server(self, url):
+        print("Sending points...")
         xyz = np.random.rand(100000, 3)
         self.viewerClient.load(xyz)
         self.viewerClient.attributes(xyz)
