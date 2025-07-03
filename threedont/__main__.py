be8cc58583c93a8fd9281517f7dba34ee0d2dd09
mcodiglione <mcodiglione@fbk.eu>
2025-07-03 15:45:09 +0200

    fix

--- threedont/__main__.py
+++ threedont/__main__.py
@@ -6,11 +6,21 @@ from .app.state import Config, AppState
 
 # Setup dependencies
 import nltk
+
 nltk.download("wordnet", user_data_dir("threedont"))
 
+from PyQt6.QtGui import QSurfaceFormat
+
+fmt = QSurfaceFormat()
+fmt.setRenderableType(QSurfaceFormat.RenderableType.OpenGL)
+fmt.setVersion(3, 2)
+fmt.setProfile(QSurfaceFormat.OpenGLContextProfile.CoreProfile)
+QSurfaceFormat.setDefaultFormat(fmt)
+
+
 def main():
-    parser = argparse.ArgumentParser(description='3Dont')
-    parser.add_argument('--test', action='store_true', help='Test the viewer')
+    parser = argparse.ArgumentParser(description="3Dont")
+    parser.add_argument("--test", action="store_true", help="Test the viewer")
     args = parser.parse_args()
 
     app_state = AppState("threedont")
@@ -22,5 +32,5 @@ def main():
     print("Application stopped gracefully")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
